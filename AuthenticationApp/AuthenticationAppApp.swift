d17ee42846e6dad1e80fa9a45a5cc1b87bc7a6cd
Vanderson Arruda <contato@vanderson.com.br>
2023-08-07 21:50:11 -0300

    Initial Commit

---
+++ AuthenticationApp/AuthenticationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationAppApp.swift
+//  AuthenticationApp
+//
+//  Created by Vanderson Arruda on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthenticationApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AuthenticationApp
+//
+//  Created by Vanderson Arruda on 07/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
